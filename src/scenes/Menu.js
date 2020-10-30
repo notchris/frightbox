67f0437642c2b9e375797d5f630e7b0ec8c5e3dc
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-10-30 14:05:59 -0400

    display frightbox title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,19 +10,18 @@ export default class Menu extends Phaser.Scene {
     }
 
     create () {
-        // Title Text
-        const title = this.add.text(240, 60, 'FRIGHT BOX', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00', align: 'center' });
-        title.setOrigin(0.5)
+        const midX = this.game.config.width / 2;
+        const midY = this.game.config.height / 2;
 
-        // Menu Items
-        const itemStart = this.add.text(240, 180, 'New Game', { fontFamily: 'Arial', fontSize: 48, color: '#ffffff', align: 'center' });
+        this.add.image(midX, midY, 'title');
+
+        const itemStart = this.add.text(midX, midY + 60, 'New Game', { fontFamily: 'Arial', fontSize: 40, color: '#ffffff', align: 'center' });
         itemStart.setOrigin(0.5)
         itemStart.setInteractive({
             useHandCursor: true
         }).on('pointerdown', (pointer, localX, localY, event) => {
             this.scene.start('SceneA')
         });
-
     }
 
     update () {
